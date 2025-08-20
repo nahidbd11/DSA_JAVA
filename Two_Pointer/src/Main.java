4b6b97b4f7ca0b486017850afcb578d7fadd7cdf
Nahid <60399063+nahidbd11@users.noreply.github.com>
2025-08-20 19:44:41 +0600

    initial commit

--- Two_Pointer/src/Main.java
+++ Two_Pointer/src/Main.java
@@ -2,8 +2,6 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-
-
          final RemoveDuplicateSortedArray removeDuplicateSortedArray=new RemoveDuplicateSortedArray();
          removeDuplicateSortedArray.removeDuplicates(new int[]{1,1,2,3,3});
     }
