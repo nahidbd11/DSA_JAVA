42013a88a8160fa95d4ea45d2bd10a10a721ccd7
Nahid <60399063+nahidbd11@users.noreply.github.com>
2025-09-09 09:05:45 +0600

    linked list problems

---
+++ LinkedList/src/MiddleNode.java
@@ -0,0 +1,49 @@
+public class MiddleNode {
+    public class ListNode {
+        int val;
+    ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    /**
+     *  slow and fast pointer approach
+     * @param head
+     * @return
+     */
+
+    public ListNode middleNode(ListNode head) {
+
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+
+        }
+
+        return slow;
+
+    }
+
+
+    /***
+     *  hasmap approach
+     * @param head
+     * @return
+     */
+/*    public ListNode middleNode(ListNode head) {
+
+        ListNode current=head;
+        int count=0;
+        HashMap<Integer,ListNode> map=new HashMap();
+        while(current!=null){
+            map.put(count++,current);
+            current=current.next;
+        }
+        Integer middle= Math.round(count/2);
+        return map.get(middle);
+
+    }*/
+}
